ffd204b055a42d2f4a44cd721ba97ef8f3253397
techsinarif <68508644+techsinarif@users.noreply.github.com>
2021-11-20 09:45:33 +0530

    Create contacts.js

---
+++ contact-list/models/contacts.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  emailId: {type: String, required: true, unique: true},
+  phoneNumber: {type: Number, required: true, unique: true}
+}, {collection: 'contacts'});
+
+const model = mongoose.model('contacts', contactSchema);
+
+module.exports = model;
